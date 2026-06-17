0af0ab75fb9f7eab8c87a66838759255a68063a8
Ekaagra Gupta <ekaagrag2006@gmail.com>
2026-06-17 12:05:46 +0530

    “new”

--- first.js
+++ first.js
@@ -1,5 +1,1 @@
-// Syntax: condition ? value_if_true : value_if_false;
-
-let age = 20;
-let allowed = (age >= 18) ? "Yes, come in!" : "Too young!"; 
-console.log(allowed); // Output: "Yes, come in!"
+console.log(typeof "76");
