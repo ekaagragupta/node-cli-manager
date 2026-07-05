e79e89f89ec7dd3a9d7b141c86e72a7afc68b128
Ekaagra Gupta <ekaagrag2006@gmail.com>
2026-07-05 16:20:45 +0530

    log

--- app.js
+++ app.js
@@ -14,5 +14,22 @@ async function main() {
     try{
         logger.log(`Command received :"${command}"with arguments: ${args.join(', ')}`);
         //log every command received with its arguments to the log file and console
+        if(!command){
+            throw new AppError("No command provided", 400);
+        }
+        switch(command){
+            case 'copy':
+                await copyFile(args[0], args[1]);
+                console.log(`COPY command executed successfully"`);
+                break;
+            case 'stats':
+                await fileStats(args[0]);
+                break;
+            default:
+                throw new AppError(`Unknown command: ${command}`, 400);
+        }
+    }catch(err){
+        if(err instanceof AppError){
+            console.error(`\n Error ($)`)
     }
 }
